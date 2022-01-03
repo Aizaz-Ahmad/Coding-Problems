8b091598fcc4938873b3145b19f06c06d252e8cb
Aizaz Ahmad <44976514+Aizaz-Ahmad@users.noreply.github.com>
2022-01-03 14:47:06 +0500

    Update twoSum.cpp

--- LeetCode/Array/twoSum.cpp
+++ LeetCode/Array/twoSum.cpp
@@ -1,1 +1,22 @@
-cout << 'abc';
+//https://leetcode.com/problems/two-sum
+#include <map>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> hash;
+        for(int i = 0; i < nums.size(); i++){
+            int reqNumber = target - nums[i];
+            auto find = hash.find(reqNumber);
+            
+            if(find != hash.end()){
+                vector<int> result;
+                result.push_back(i);
+                result.push_back(find->second);
+                return result;
+            }
+            else
+                hash.insert({nums[i], i});
+        }
+        return vector<int>();
+    }
+};

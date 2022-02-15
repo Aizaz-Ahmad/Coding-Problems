3ed21f838da7c58b2720ebe6166e4a0e13f91f7d
Aizaz Ahmad <44976514+Aizaz-Ahmad@users.noreply.github.com>
2022-02-15 16:19:09 +0500

    Create spiral-matrix.cpp

---
+++ spiral-matrix.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/spiral-matrix/submissions/
+
+class Solution {
+private:
+    void spiralOrder(vector<vector<int>>& matrix, vector<int>& res, int sr, int sc,
+                    int er, int ec){
+        if(sr > er || sc > ec) return;
+        
+        for(int i = sc; i <= ec; i++)
+            res.push_back(matrix[sr][i]);
+        sr++;
+        for(int i = sr; i <= er;i++)
+            res.push_back(matrix[i][ec]);
+        ec--;
+        if(sr > er) return;
+        for(int i = ec; i >= sc; i--)
+            res.push_back(matrix[er][i]);
+        er--;
+        if(sc > ec) return;
+        for(int i = er; i >= sr; i--)
+            res.push_back(matrix[i][sc]);
+        sc++;
+        spiralOrder(matrix, res, sr, sc, er, ec);
+    }
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int rows = matrix.size(), cols = matrix[0].size();
+        
+        spiralOrder(matrix, res, 0, 0, rows - 1, cols - 1);
+        
+        return res;
+    }
+};

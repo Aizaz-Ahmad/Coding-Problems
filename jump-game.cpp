77e6a7cee1615fb3715d12523be70cedfc728f64
Aizaz Ahmad <44976514+Aizaz-Ahmad@users.noreply.github.com>
2022-02-15 22:57:05 +0500

    Update jump-game.cpp

--- jump-game.cpp
+++ jump-game.cpp
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/jump-game/
+// https://leetcode.com/problems/jump-game/discuss/21121/C%2B%2B-elegant-solution-O(n)-time.
+//https://leetcode.com/problems/jump-game/discuss/20917/Linear-and-simple-solution-in-C%2B%2B
 
 class RecursiveSolution {
 private: 
@@ -19,3 +21,10 @@ public:
         return canJump(nums, 0, lastPos);
     }
 };
+
+bool canJump(int A[], int n) {
+    int i = 0;
+    for (int reach = 0; i < n && i <= reach; ++i)
+        reach = max(i + A[i], reach);
+    return i == n;
+}

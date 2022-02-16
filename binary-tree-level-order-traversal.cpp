dbc9f257e456e5eaba5a57b37c364dd7cc9a028c
Aizaz Ahmad <44976514+Aizaz-Ahmad@users.noreply.github.com>
2022-02-16 21:02:48 +0500

    Update binary-tree-level-order-traversal.cpp

--- binary-tree-level-order-traversal.cpp
+++ binary-tree-level-order-traversal.cpp
@@ -34,3 +34,27 @@ public:
         return result;
     }
 };
+
+
+class ImprovedSolution {
+private: 
+    void levelOrder(TreeNode* root, int depth, vector<vector<int>>& result){
+        if(!root) return;
+        //increase the size, instead of getting the height
+        if(result.size() == depth)
+            result.push_back(vector<int>());
+        
+        result[depth].push_back(root->val);
+        levelOrder(root->left, depth + 1, result);
+        levelOrder(root->right, depth + 1, result);
+    }
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        vector<vector<int>> result;
+        
+        levelOrder(root, 0, result);
+        
+        return result;
+    }
+};

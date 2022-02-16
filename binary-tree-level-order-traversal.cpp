bc3f0ae7170bd46454d5835ac35a8fc6e714394a
Aizaz Ahmad <44976514+Aizaz-Ahmad@users.noreply.github.com>
2022-02-16 20:57:29 +0500

    Create binary-tree-level-order-traversal.cpp

---
+++ binary-tree-level-order-traversal.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private: 
+    int getHeightOfTree(TreeNode *root, int depth){
+        if(root == NULL) return depth;
+        return max(getHeightOfTree(root->left, depth + 1), getHeightOfTree(root->right, depth + 1)); 
+    }
+    void levelOrder(TreeNode* root, int depth, vector<vector<int>>& result){
+        if(!root) return;
+        result[depth].push_back(root->val);
+        levelOrder(root->left, depth + 1, result);
+        levelOrder(root->right, depth + 1, result);
+    }
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        int height = getHeightOfTree(root, 0);
+        if(!height) return vector<vector<int>>();
+        
+        vector<vector<int>> result(height, vector<int>());
+        
+        levelOrder(root, 0, result);
+        
+        return result;
+    }
+};

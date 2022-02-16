64eb219893f505e5cd596b8fd950172dce9ee4ce
Aizaz Ahmad <44976514+Aizaz-Ahmad@users.noreply.github.com>
2022-02-16 14:32:24 +0500

    Update unique-paths.cpp

--- unique-paths.cpp
+++ unique-paths.cpp
@@ -9,3 +9,23 @@ public:
         return uniquePaths(m - 1, n) + uniquePaths(m, n - 1); 
     }
 };
+
+class DPSolution {
+public:
+    int uniquePaths(int m, int n) {
+        //Each cell will contain the number of unique paths from that position
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        //initialize last column and last row with 1 -
+        //because there is only one unique path to reach end when matrix is row or column
+        for(int i = 0; i < n; i++)
+            dp[m - 1][i] = 1;
+        for(int i = 0; i < m; i++)
+            dp[i][n - 1] = 1;
+        //moving back from right to left and then bottom to top, and summing the bottom and right cell.
+        for(int i = m - 2; i >=0; i--)
+            for(int j = n - 2; j >= 0; j--)
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+        //solution will be in the first cell
+        return dp[0][0];
+    }
+};
